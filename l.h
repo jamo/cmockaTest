951b11e110a3a04bee059e719b3c04b103b1c81e
Jarmo Isotalo <jamo@isotalo.fi>
2013-01-27 21:59:49 +0200

    hajoo

---
+++ l.h
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct dList {
+  int x;
+  struct dList *next;
+  struct dList *prev;
+} dList;
+
+
+dList* insertElementD(dList *L, dList *p, int value);
+
+int printElementsD(dList *L);
+
+dList* createElem(int value, dList *pr, dList *nx);
+
+dList* createElemNo(int value);
+
+dList* deleteElementD(dList *L, dList *p);
+
+dList* getMin(dList *L);
+
+dList* clone(dList *node);
+
+void swap(dList *eka, dList *toka);
+
+int orderListD(dList *L);
+
+dList* mergeListsD(dList *L1, dList *L2);
+
---
+++ mytest.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include "cmocka.h"
+
+#include "l.h"
+
+
+int main(){
+
+  createElem(5,NULL,NULL);
+  //createElemNo(22);
+/*    const UnitTest tests[] = {
+        unit_test(Test0),
+        unit_test(Test1),
+    };
+    return run_tests(tests);
+    */
+  return 0;
+}
